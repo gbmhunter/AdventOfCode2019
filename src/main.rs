a66f66eebdd29a43399077b06a59a406bb7fdf7e
Geoffrey Hunter <ghunter@urthecast.com>
2019-12-09 11:46:58 +1300

    Added day 1 part 2.

--- src/main.rs
+++ src/main.rs
@@ -13,10 +13,30 @@ fn main() -> io::Result<()> {
     }
 
     let mut fuel_sum = 0;
+    let mut fuel_sum_with_fuel: i32 = 0;
     for module_mass in module_masses {
-        fuel_sum += (module_mass/3) - 2;
+        let fuel_for_module = (module_mass/3) - 2;
+        fuel_sum += fuel_for_module;
+        let extra_fuel = calc_extra_fuel(fuel_for_module as i32);
+        fuel_sum_with_fuel += (fuel_for_module as i32) + extra_fuel;
     }
 
-    println!("fuel_sum = {}", fuel_sum);
+    println!("part 1: fuel_sum = {}", fuel_sum);
+
+    println!("part 2: fuel_sum_with_fuel = {}", fuel_sum_with_fuel);
+
+
     Ok(())
+}
+
+fn calc_extra_fuel(fuel_weight: i32) -> i32 {
+    // println!("calc_extra_fuel() called with fuel_weight={}", fuel_weight);
+    let mut extra_fuel: i32 = (fuel_weight)/3 - 2;
+    if extra_fuel > 0 {
+        extra_fuel += calc_extra_fuel(extra_fuel);
+    } else {
+        extra_fuel = 0
+    }
+    // println!("total extra fuel={}", extra_fuel);
+    extra_fuel
 }
