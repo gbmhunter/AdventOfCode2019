725788bc7f8836f02a6f9a19a014d1444007a6e6
Geoffrey Hunter <gbmhunter@gmail.com>
2019-12-14 08:21:09 +1300

    Added solution to day 02 part 2.

---
+++ README.md
@@ -0,0 +1,10 @@
+## Overview
+
+[Advent of Code 2019](https://adventofcode.com/2019/).
+
+## To Run
+
+```
+cd advent_of_code_2019
+cargo run
+```
--- advent_of_code_2019/src/day02/mod.rs
+++ advent_of_code_2019/src/day02/mod.rs
@@ -4,40 +4,66 @@ pub fn main() {
     let contents = fs::read_to_string("src/day02/input.txt").unwrap();
 
     let split = contents.split(",");
-    let mut op_codes: Vec<u32> = Vec::new();
+    let mut memory: Vec<u32> = Vec::new();
     for num_as_string in split {
-        op_codes.push(num_as_string.parse().unwrap());
+        memory.push(num_as_string.parse().unwrap());
     }
 
-    // Manual replacement as per instructions
-    op_codes[1] = 12;
-    op_codes[2] = 2;
 
+    let mut memory_copy = memory.clone();
+    // Manual replacement as per instructions
+    memory_copy[1] = 12;
+    memory_copy[2] = 2;
+    memory_copy = run_computer(memory_copy);
+
+    println!("day 02 part 1: Value at memory 0 = {}", memory_copy[0]);
+
+    let mut done = false;
+    for i in 0..100 {
+        // println!("i = {}", i);
+        for j in 0..100 {
+            memory_copy = memory.clone();
+            memory_copy[1] = i;
+            memory_copy[2] = j;
+            memory_copy = run_computer(memory_copy);
+            if memory_copy[0] == 19690720 {
+                println!("day 02 part 2: 100*noun + verb = {}", 100*i + j);
+                done = true;
+            }
+            if done {
+                break;
+            }
+        }
+        if done {
+            break;
+        }
+    }
+}
+
+pub fn run_computer(mut memory: Vec<u32>) -> Vec<u32> {
     let mut curr_pos = 0;
     while true {
-        let op_code = op_codes[curr_pos];
+        let op_code = memory[curr_pos];
         if op_code == 1 {
-            let operand_1 = op_codes[op_codes[curr_pos + 1] as usize];
-            let operand_2 = op_codes[op_codes[curr_pos + 2] as usize];
-            let output_idx = op_codes[curr_pos + 3] as usize;
+            let operand_1 = memory[memory[curr_pos + 1] as usize];
+            let operand_2 = memory[memory[curr_pos + 2] as usize];
+            let output_idx = memory[curr_pos + 3] as usize;
 
             let output_value = operand_1 + operand_2;
-            op_codes[output_idx] = output_value;
+            memory[output_idx] = output_value;
         } else if op_code == 2 {
-            let operand_1 = op_codes[op_codes[curr_pos + 1] as usize];
-            let operand_2 = op_codes[op_codes[curr_pos + 2] as usize];
-            let output_idx = op_codes[curr_pos + 3] as usize;
+            let operand_1 = memory[memory[curr_pos + 1] as usize];
+            let operand_2 = memory[memory[curr_pos + 2] as usize];
+            let output_idx = memory[curr_pos + 3] as usize;
 
             let output_value = operand_1 * operand_2;
-            op_codes[output_idx] = output_value;
+            memory[output_idx] = output_value;
         } else if op_code == 99 {
             break;
         } else {
-            println!("ERROR!")
+            panic!("Unrecognized op code.");
         }
         curr_pos += 4;
     }
-
-
-    println!("day 02 part 1: Value at op code 0 = {}", op_codes[0]);
+    return memory;
 }
