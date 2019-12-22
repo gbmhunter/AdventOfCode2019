1cab511434919ae3aa0e6d9f58a7abf8fbd1e41e
Geoffrey Hunter <gbmhunter@gmail.com>
2019-12-22 18:50:35 +1300

    Added solution for day 05 part 2.

--- advent_of_code_2019/src/day05/mod.rs
+++ advent_of_code_2019/src/day05/mod.rs
@@ -11,11 +11,15 @@ pub fn main() {
     }
 
     let mut memory_copy = memory.clone();
-    let ret_vals = run_computer(memory_copy);
+    let ret_vals = run_computer(memory_copy, 1);
     println!("day 05 part 1: diagnostic code = {}", ret_vals.1[ret_vals.1.len() - 1]);
+
+    memory_copy = memory.clone();
+    let ret_vals = run_computer(memory_copy, 5);
+    println!("day 05 part 2: diagnostic code = {}", ret_vals.1[ret_vals.1.len() - 1]);
 }
 
-fn run_computer(mut memory: Vec<i32>) -> (Vec<i32>, Vec<i32>) {
+fn run_computer(mut memory: Vec<i32>, input: i32) -> (Vec<i32>, Vec<i32>) {
     // println!("run_computer() called.");
     let mut curr_pos: usize = 0;
     let mut outputs: Vec<i32> = Vec::new();
@@ -36,36 +40,88 @@ fn run_computer(mut memory: Vec<i32>) -> (Vec<i32>, Vec<i32>) {
         let output_value: i32;
         if op_code == 1 {
             // println!("Got addition opcode.");
-            let output_idx = memory[curr_pos + 3] as usize;
             // println!("output_idx = {}", output_idx);
-            let operands = parse_2_params(&memory, curr_pos);
-            output_value = operands.0 + operands.1;
-            memory[output_idx] = output_value;
+            let param_1 = parse_param(&memory, curr_pos, 1);
+            let param_2 = parse_param(&memory, curr_pos, 2);
+            let param_3 = memory[curr_pos + 3];
+            output_value = param_1 + param_2;
+            memory[param_3 as usize] = output_value;
             curr_pos += 4;
         } else if op_code == 2 {
             let output_idx = memory[curr_pos + 3] as usize;
-            let operands = parse_2_params(&memory, curr_pos);
-            output_value = operands.0 * operands.1;
-            memory[output_idx] = output_value;
+            let param_1 = parse_param(&memory, curr_pos, 1);
+            let param_2 = parse_param(&memory, curr_pos, 2);
+            let param_3 = memory[curr_pos + 3];
+            output_value = param_1 * param_2;
+            memory[param_3 as usize] = output_value;
             curr_pos += 4;
         } else if op_code == 3 {
             // Take input
             // println!("Got input opcode.");
-            let input = 1;
             let output_idx = memory[curr_pos + 1] as usize;
+            // println!("Saving input {} at memory position {}", input, output_idx);
             memory[output_idx] = input;
             curr_pos += 2;
         } else if op_code == 4 {
             let output = memory[memory[curr_pos + 1] as usize];
-            // println!("Printing output: {}", output);
+            // println!("Printing output at memory location {}: {}", memory[curr_pos + 1], output);
             outputs.push(output);
             curr_pos += 2;
+        } else if op_code == 5 {
+            // println!("Got jump-if-true opcode.");
+            let param_1 = parse_param(&memory, curr_pos, 1);
+            let param_2 = parse_param(&memory, curr_pos, 2);
+            if param_1 != 0 {
+                // println!("Jumping to instruction {}", param_2);
+                curr_pos = param_2 as usize;
+            } else {
+                // println!("Not jumping.");
+                curr_pos += 3;
+            } 
+        } else if op_code == 6 {
+            // println!("Got jump-if-false opcode.");
+            let param_1 = parse_param(&memory, curr_pos, 1);
+            let param_2 = parse_param(&memory, curr_pos, 2);
+            if param_1 == 0 {
+                // println!("Jumping to instruction {}", param_2);
+                curr_pos = param_2 as usize;
+            } else {
+                // println!("Not jumping.");
+                curr_pos += 3;
+            } 
+        } else if op_code == 7 {
+            // println!("Got less-than opcode.");
+            let param_1 = parse_param(&memory, curr_pos, 1);
+            let param_2 = parse_param(&memory, curr_pos, 2);
+            let param_3 = memory[curr_pos + 3];
+            if param_1 < param_2 {
+                // println!("param_1 {} is less than param_2 {}, storing 1 at {}", param_1, param_2, param_3);
+                memory[param_3 as usize] = 1;
+            } else {
+                // println!("param_1 {} is NOT less than param_2 {}, storing 0 at {}", param_1, param_2, param_3);
+                memory[param_3 as usize] = 0;
+            } 
+            curr_pos += 4;
+        } else if op_code == 8 {
+            // println!("Got equal-to opcode, curr_pos = {}.", curr_pos);
+            let param_1 = parse_param(&memory, curr_pos, 1);
+            let param_2 = parse_param(&memory, curr_pos, 2);
+            let param_3 = memory[curr_pos + 3];
+            if param_1 == param_2 {
+                // println!("param_1 {} is equal to param_2 {}, storing 1 at {}", param_1, param_2, param_3);
+                memory[param_3 as usize] = 1;
+            } else {
+                // println!("param_1 {} is NOT equal to param_2 {}, storing 0 at {}", param_1, param_2, param_3);
+                memory[param_3 as usize] = 0;
+            } 
+            curr_pos += 4;
         } else if op_code == 99 {
             // println!("Halting.");
             break;
         } else {
             panic!("Unrecognized op code.");
         }
+        // println!("memory after performing instruction = {:?}", memory);
     }
     return (memory, outputs);
 }
@@ -80,6 +136,31 @@ fn number_to_vec(n: i32) -> Vec<i32> {
     digits.push(n);
     digits.reverse();
     digits
+}
+
+fn parse_param(memory: &Vec<i32>, curr_pos: usize, param_num: usize) -> i32 {
+    let op_code_and_parameter_mode = memory[curr_pos];
+    let mut op_code_and_parameters_mode_vec = number_to_vec(op_code_and_parameter_mode as i32);
+    op_code_and_parameters_mode_vec.reverse();
+
+    let mut param_mode = 0;
+    if op_code_and_parameters_mode_vec.len() >= 2 + param_num {
+        param_mode = op_code_and_parameters_mode_vec[1 + param_num];
+    }
+
+    let operand: i32;
+    if param_mode == 0 {
+        // println!("Looking up value at address = {}", memory[curr_pos + param_num]);
+        operand = memory[memory[curr_pos + param_num] as usize];
+    } else if param_mode == 1 {
+        operand = memory[curr_pos + param_num];
+    } else {
+        panic!("param_mode not supported.");
+    }
+
+    // println!("Got param_num = {}, param value = {}, param_mode = {}, curr_pos = {}, memory = {:?}", param_num, operand, param_mode, curr_pos, memory);
+    
+    return operand;
 }
 
 fn parse_2_params(memory: &Vec<i32>, curr_pos: usize) -> (i32, i32) {
