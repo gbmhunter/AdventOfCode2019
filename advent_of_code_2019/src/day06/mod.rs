6018ea28cf8496a95559f835a4f5d55c4584245d
Geoffrey Hunter <gbmhunter@gmail.com>
2019-12-24 08:37:19 +1300

    Started working on day 06 part 1.

---
+++ advent_of_code_2019/src/day06/mod.rs
@@ -0,0 +1,56 @@
+use std::fs::File;
+use std::io::{BufRead, BufReader};
+use std::collections::HashMap;
+
+struct Planet<'a> {
+    name: String,
+    orbits: Option<&'a mut Self>,
+    orbiting_children: Vec<&'a mut Self>,
+}
+
+
+pub fn main() {
+    let filename = "src/day06/input.txt";
+    // Open the file in read-only mode (ignoring errors).
+    let file = File::open(filename).unwrap();
+    let reader = BufReader::new(file);
+
+    let mut name_to_planet_map: HashMap<String, Planet> = HashMap::new();
+
+    // Read the file line by line using the lines() iterator from std::io::BufRead.
+    for (index, line) in reader.lines().enumerate() {
+        let line = line.unwrap(); // Ignore errors.
+        // Show the line and its number.
+        println!("{}. {}", index + 1, line);
+        let parts: Vec<&str> = line.split(")").collect();
+        let central_planet_name = parts[0].to_string();
+        let orbiting_planet_name = parts[1].to_string();
+
+        // Check if central planet already exists
+        if !name_to_planet_map.contains_key(&central_planet_name) {
+            println!("Planet '{}' does not exist, creating...", central_planet_name);
+            let mut planet = Planet {
+                name: central_planet_name.clone(),
+                orbits: None,
+                orbiting_children: Vec::new(),
+            };
+            name_to_planet_map.insert(planet.name.clone(), planet);
+        }
+        // Check if orbiting already exists
+        if !name_to_planet_map.contains_key(&orbiting_planet_name) {
+            println!("Planet '{}' does not exist, creating...", orbiting_planet_name);
+            let mut planet = Planet {
+                name: orbiting_planet_name.clone(),
+                orbits: None,
+                orbiting_children: Vec::new(),
+            };
+            name_to_planet_map.insert(planet.name.clone(), planet);
+        }
+
+        // Add link from parent planet
+        let central_planet = &mut name_to_planet_map[&central_planet_name];
+        let orbiting_planet = &mut name_to_planet_map[&orbiting_planet_name];
+        central_planet.orbiting_children.push(orbiting_planet);
+
+    }
+}
--- advent_of_code_2019/src/main.rs
+++ advent_of_code_2019/src/main.rs
@@ -1,17 +1,21 @@
+#![allow(dead_code)]
 use std::io::{self};
+
 
 mod day01;
 mod day02;
 mod day03;
 mod day04;
 mod day05;
+mod day06;
 
 fn main() -> io::Result<()> {
     // day01::main();
     // day02::main();
     // day03::main();
     // day04::main();
-    day05::main();
+    // day05::main();
+    day06::main();
     Ok(())
 
 }
