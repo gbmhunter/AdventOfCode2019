d642d8f6c6d428371ea75259c5fc5d5346adbee8
Geoffrey Hunter <gbmhunter@gmail.com>
2019-12-26 09:54:19 +1300

    Added solution for day 06 part 1.

--- advent_of_code_2019/src/day06/mod.rs
+++ advent_of_code_2019/src/day06/mod.rs
@@ -15,42 +15,30 @@ pub fn main() {
     let file = File::open(filename).unwrap();
     let reader = BufReader::new(file);
 
-    let mut name_to_planet_map: HashMap<String, Planet> = HashMap::new();
+    let mut child_to_parent_map: HashMap<String, String> = HashMap::new();
 
     // Read the file line by line using the lines() iterator from std::io::BufRead.
     for (index, line) in reader.lines().enumerate() {
         let line = line.unwrap(); // Ignore errors.
         // Show the line and its number.
-        println!("{}. {}", index + 1, &line);
+        // println!("{}. {}", index + 1, &line);
         let parts: Vec<&str> = line.split(")").collect();
         let central_planet_name = parts[0].to_string();
         let orbiting_planet_name = parts[1].to_string();
 
-        // Check if central planet already exists
-        if !name_to_planet_map.contains_key(&central_planet_name) {
-            println!("Planet '{}' does not exist, creating...", &central_planet_name);
-            let mut planet = Planet {
-                name: central_planet_name.clone(),
-                orbits: None,
-                orbiting_children: Vec::new(),
-            };
-            name_to_planet_map.insert(planet.name.clone(), planet);
-        }
-        // Check if orbiting already exists
-        if !name_to_planet_map.contains_key(&orbiting_planet_name) {
-            println!("Planet '{}' does not exist, creating...", &orbiting_planet_name);
-            let mut planet = Planet {
-                name: orbiting_planet_name.clone(),
-                orbits: None,
-                orbiting_children: Vec::new(),
-            };
-            name_to_planet_map.insert(planet.name.clone(), planet);
-        }
-
-        // Add link from parent planet
-        let mut central_planet = name_to_planet_map.get_mut(&central_planet_name).unwrap();
-        let mut orbiting_planet = name_to_planet_map.get_mut(&orbiting_planet_name).unwrap();
-        // central_planet.orbiting_children.push(orbiting_planet);
+        child_to_parent_map.insert(orbiting_planet_name, central_planet_name);      
 
     }
+
+    let mut count = 0;
+    for mut child in child_to_parent_map.keys() {
+        // println!("child = {}", child);
+        while let Some(parent) = child_to_parent_map.get(child) {
+            // println!("parent = {}", parent);
+            child = parent;
+            count += 1;
+        }
+    }
+    println!("day 06 part 1: count = {}", count);
+
 }
